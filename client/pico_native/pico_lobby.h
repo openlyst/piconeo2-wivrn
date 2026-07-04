1242880f59c3aeb51bbf2b93ca2a59b3087928e9
HttpAnimations <shawndouglasyt@gmail.com>
2026-07-03 23:15:06 -0700

    Remove lobby

--- client/pico_native/pico_lobby.h
+++ client/pico_native/pico_lobby.h
@@ -12,35 +12,10 @@ class pico_lobby
 	GLint mvp_uniform = -1;
 	GLint color_uniform = -1;
 
-	// UI texture program (textured quad)
-	GLuint tex_program = 0;
-	GLuint ui_fbo = 0;
-	GLuint ui_texture = 0;
-	GLuint quad_vbo = 0;
-	GLint tex_pos_attrib = -1;
-	GLint tex_uv_attrib = -1;
-	GLint tex_mvp_uniform = -1;
-	GLint tex_sampler_uniform = -1;
-
-	static constexpr int ui_w = 1024;
-	static constexpr int ui_h = 1024;
-
-	// TV panel state (world-locked)
-	float tv_pos[3] = {0, 1.5f, -2.0f};
-	float tv_yaw = 0.0f;
-	bool tv_placed = false;
-	bool prev_grip[2] = {false, false};
-	bool prev_stick_click[2] = {false, false};
-
 	int eye_width = 0;
 	int eye_height = 0;
 	bool initialized = false;
 
-	void render_ui();
-	void draw_quad(const float head_orient[4], const float head_pos[3],
-	               const XrFovf & fov, float ipd, int eye);
-	void update_interaction(const float head_orient[4], const float head_pos[3],
-	                        const controller_sample controllers[2]);
 
 public:
 	pico_lobby() = default;
